da9f353a4bd9e77b170d33d3c4d8f2153aa95bb4
unknown <tugberkurganci@gmail.com>
2024-07-26 08:44:04 +0300

    deploy time

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+import { fileURLToPath, URL } from "node:url";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/prvt2/',  // GitHub Pages'de proje yolu
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name][extname]',
-        chunkFileNames: 'chunks/[name].js',
-        entryFileNames: 'js/[name].js',
-      }
+  plugins: [react(),
+  ],
+  server: {
+    base: "/prvt2/",
+    proxy: {
+      '/api': 'http://localhost:8080',
     }
   },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
-  }
-});
+  
+  
+})
