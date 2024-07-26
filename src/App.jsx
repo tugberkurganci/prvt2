f7c1efc965b3277ce8b40b707979de24cffd5f71
unknown <tugberkurganci@gmail.com>
2024-07-26 07:36:44 +0300

    deploy time

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css'
 import PharmacyView from './pages/PharmacyView';
 import TaxiCall from './pages/TaxiCall';
